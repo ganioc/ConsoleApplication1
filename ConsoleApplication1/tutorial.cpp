366358166f55850465bb0a70f70b3ffdaf0ea3ef
yango <o3.yang@hotmail.com>
2024-02-09 07:53:16 +0800

    Update doSomeWork()

--- ConsoleApplication1/tutorial.cpp
+++ ConsoleApplication1/tutorial.cpp
@@ -6,10 +6,10 @@ namespace tutorial {
 		std::cout << "G-> " << i << std::endl;
 	}
 	void doSomeWork() {
-		//auto compare = [](int i, int j) {
-		//	return i < j;
-		//};
-		//std::cout << compare(3, 2) << std::endl;
+		auto compare = [](int i, int j) {
+			return i < j;
+		};
+		std::cout << compare(3, 2) << std::endl;
 		std::cout << "some work" << std::endl;
 	}
 }
