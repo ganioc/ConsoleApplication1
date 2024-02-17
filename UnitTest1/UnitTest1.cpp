0dc166f6465218d907638bb0d55dd3c198d9d4da
yango <o3.yang@hotmail.com>
2024-02-17 22:01:03 +0800

    update Unit Test vector

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -2,7 +2,7 @@
 #include "CppUnitTest.h"
 #include "../ConsoleApplication1/compute.h"
 
-
+#include <vector>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -14,6 +14,12 @@ namespace UnitTest1
 		TEST_METHOD(TESTAddTwo)
 		{
 			Assert::AreEqual(add_two(10), 12);
+		}
+		TEST_METHOD(TESTVector) {
+			std::vector<int> v(10);
+
+			Assert::AreEqual(10, (int)(v.size()));
+			Assert::AreEqual(10, (int)v.capacity());
 		}
 	};
 }
