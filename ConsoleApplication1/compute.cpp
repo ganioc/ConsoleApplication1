7265a7428b544cc47c017b9cb920010b6364859b
yango <o3.yang@hotmail.com>
2024-03-25 22:15:21 +0800

    Update , function definition should only in header file. Header file only have class definition.

--- ConsoleApplication1/compute.cpp
+++ ConsoleApplication1/compute.cpp
@@ -7,10 +7,30 @@ namespace compute {
 		return num + 2;
 	}
 
+	void getFromInput(const char* msg, string& input) {
+		const int MAX_LINE_LENGTH = 80;
+		char buf[MAX_LINE_LENGTH] = { 0 };
+
+		//	if (msg != NULL) {
+		//		cout << msg << " : ";
+		//	}
+		//	else {
+		//		cout << "Enter string: ";
+		//	}
+
+		//	cin.getline(buf, MAX_LINE_LENGTH);
+		//	if (cin.eof()) {
+		//		throw DSPInputException();
+		//	}
+		//	input = buf;
+	}
+
 	void testGetInput() {
 		string strName;
 
 		cout << "test getFromInput()" << "\n";
+
+		// getFromInput("Enter your name", strName);
 
 		// Get Parameter from user
 		//do {
--- ConsoleApplication1/compute.h
+++ ConsoleApplication1/compute.h
@@ -21,23 +21,7 @@ namespace compute {
 	// getInput
 	// Throws: DSPException
 	//
-	//void getFromInput(const char* msg, string& input) {
-	//	const int MAX_LINE_LENGTH = 80;
-	//	char buf[MAX_LINE_LENGTH] = { 0 };
-
-	//	if (msg != NULL) {
-	//		cout << msg << " : ";
-	//	}
-	//	else {
-	//		cout << "Enter string: ";
-	//	}
-
-	//	cin.getline(buf, MAX_LINE_LENGTH);
-	//	if (cin.eof()) {
-	//		throw DSPInputException();
-	//	}
-	//	input = buf;
-	//}
+	void getFromInput(const char* msg, string& input);
 
 	//class DSPFile {
 	//private:
