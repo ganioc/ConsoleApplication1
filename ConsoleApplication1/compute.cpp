3917131a8988950876d251f2805912bf098667b8
yango <o3.yang@hotmail.com>
2024-03-26 14:46:03 +0800

    Update DSPFile class

--- ConsoleApplication1/compute.cpp
+++ ConsoleApplication1/compute.cpp
@@ -7,22 +7,22 @@ namespace compute {
 		return num + 2;
 	}
 
-	void getFromInput(const char* msg, string& input) {
+	void getInput(const char* msg, string& input) {
 		const int MAX_LINE_LENGTH = 80;
 		char buf[MAX_LINE_LENGTH] = { 0 };
 
-		//	if (msg != NULL) {
-		//		cout << msg << " : ";
-		//	}
-		//	else {
-		//		cout << "Enter string: ";
-		//	}
+		if (msg != NULL) {
+			cout << msg << " : ";
+		}
+		else {
+			cout << "Enter string: ";
+		}
 
-		//	cin.getline(buf, MAX_LINE_LENGTH);
-		//	if (cin.eof()) {
-		//		throw DSPInputException();
-		//	}
-		//	input = buf;
+		cin.getline(buf, MAX_LINE_LENGTH);
+		if (cin.eof()) {
+			throw DSPInputException();
+		}
+		input = buf;
 	}
 
 	void testGetInput() {
@@ -33,8 +33,57 @@ namespace compute {
 		// getFromInput("Enter your name", strName);
 
 		// Get Parameter from user
-		//do {
-		//	getFromInput("Enter your name", strName);
-		//} while (strName.empty());
+		do {
+			getInput("Enter your name", strName);
+		} while (strName.empty());
+
+		cout << "The name is: " << strName << endl;
+	}
+
+	// class DSPFile
+	DSPFile::DSPFile() : m_type( DSPFILETYPE::UNKNOWN_TYPE),
+		m_elementSize(0),
+		m_numRecords(0),
+		m_recLen(0),
+		m_trailer(NULL),
+		m_readOnly(true),
+		m_posBeginData(0),
+		m_singleElement(NULL) {
+
+	}
+	DSPFile::~DSPFile() {
+		close();
+		empty();
+	}
+	void DSPFile::empty() {
+		m_type = DSPFILETYPE::UNKNOWN_TYPE;
+		m_elementSize = 0;
+		m_numRecords = 0;
+		m_recLen = 0;
+		m_readOnly = true;
+
+		if (m_singleElement != NULL) {
+			delete[] m_trailer;
+			m_trailer = NULL;
+		}
+
+		if (m_fs.is_open()) {
+			m_fs.close();
+		}
+	}
+	bool DSPFile::isFound(const char* name) {
+		// Validate filename
+		if (name == NULL) {
+			throw DSPParamException();
+		}
+
+		// Open the file
+		fstream fs;
+		fs.open(name, ios::in | ios::_Nocreate | ios::binary);
+		if (fs.fail()) {
+			return false;
+		}
+		fs.close();
+		return true;
 	}
 }
--- ConsoleApplication1/compute.h
+++ ConsoleApplication1/compute.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -16,12 +17,18 @@ namespace compute {
 			return "DSP input exception";
 		}
 	};
+	class DSPParamException : public exception {
+	public:
+		virtual const char* what() const throw() {
+			return "DSP params exception";
+		}
+	};
 
 	//
 	// getInput
 	// Throws: DSPException
 	//
-	void getFromInput(const char* msg, string& input);
+	void getInput(const char* msg, string& input);
 
 	//class DSPFile {
 	//private:
@@ -29,7 +36,117 @@ namespace compute {
 	//	
 	//};
 
+	template <class Type>
+	void getInput(const char* msg, Type& input, Type rangeMin, Type rangeMax) {
+		if (rangeMin > rangeMax) {
+			throw DSPParamException();
+		}
+
+		do {
+			if (msg != NULL) {
+				cout << msg << " [" << rangeMin << ".." << rangeMax << "] : ";
+			}
+			else {
+				cout << "Enter number [" << rangeMin << ".." << rangeMax << "] : ";
+			}
+
+			cin >> input;
+
+			if (cin.eof()) {
+				throw DSPInputException();
+			}
+		} while (input < rangeMin || input > rangeMax);
+
+		// Read trailing newline
+		cin.get();
+	}
+
 	void testGetInput();
+
+
+	enum class DSPFILETYPE {
+		UNSIGNED_CHAR = 0,  // 0
+		UNSIGNED_SHORT = 1, // 1
+		UNSIGNED_LONG,  // 2
+		FLOAT,          // 3
+		SIGNED_CHAR,    // 4
+		SIGNED_SHORT,   // 5
+		SIGNED_LONG,    // 6
+		DOUBLE,         // 7
+		UNSIGNED_INT,   // 8
+		SIGNED_INT,     // 9
+		COMPLEX,        // 10
+		UNKNOWN_TYPE = 0xFF
+	};
+
+	// DSPFile
+	class DSPFile {
+	private:
+		// Data type 0-7 as defined above
+		DSPFILETYPE m_type;
+
+		// Size of each element
+		unsigned char m_elementSize;
+
+		// Number of records
+		int m_numRecords;
+
+		// Number of elements in each record
+		int m_recLen;
+
+		// Pointer to trailer text
+		char* m_trailer;
+
+		// File data
+		fstream m_fs;
+		bool m_readOnly;
+		streampos m_posBeginData;
+
+		// Buffer for reading single elements
+		unsigned char* m_singleElement;
+
+		/////////////////////
+		// Helper functions
+		/////////////////////
+
+		// Clear data
+		void empty();
+	
+	public:
+		DSPFile();
+		~DSPFile();
+
+		/////////////////////
+		// File operations
+		/////////////////////
+		static bool isFound(const char* name);
+		bool hasData() {
+			return ((m_numRecords * m_recLen) != 0);
+		}
+		void openRead(const char* name);
+		void openWrite(const char* name);
+		void close();
+
+		/////////////////////
+		// Get file format
+		/////////////////////
+		DSPFILETYPE getType() {
+			return m_type;
+		}
+		int getNumRecords() {
+			return m_numRecords;
+		}
+		int getRecLen() {
+			return m_recLen;
+		}
+		//////////////////////////////
+		// Get and Set file contents
+		//////////////////////////////
+		void getTrailer(string& str);
+		void setTrailer(const char* trailer);
+		void seek(int len = 0, int rec = 0);
+
+	};
 }
 
 
